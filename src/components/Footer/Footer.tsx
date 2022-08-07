3011dd9fecadefb2e6275ab85fd83fb86a682033
cherry <82304659+rainbowfieldholograph@users.noreply.github.com>
2022-08-08 02:20:32 +0900

    footer update

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,8 +1,9 @@
+import styles from './Footer.module.css';
 import type { Component } from 'solid-js';
 
 export const Footer: Component = () => {
   return (
-    <footer>
+    <footer class={styles.footer}>
       <a href="https://github.com/rainbowfieldholograph">Github</a>
     </footer>
   );
