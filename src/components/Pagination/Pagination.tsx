54999a0946592d38c7ab265e4b4b3303263b283d
cherry <82304659+rainbowfieldholograph@users.noreply.github.com>
2022-08-11 19:09:29 +0900

    pagination update

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,4 +1,5 @@
 import { currentPage, setCurrentPage } from '../../store';
+import { ArrowLeftIcon } from '../icons/ArrowLeftIcon';
 import styles from './Pagination.module.css';
 import type { Component } from 'solid-js';
 
@@ -16,13 +17,13 @@ export const Pagination: Component = () => {
 
   return (
     <div class={styles.wrapper}>
-      <span class={styles.current}>{currentPage()}</span>
+      <span class={styles.current}>Current page: {currentPage()}</span>
       <div class={styles.buttons}>
         <button class={styles.button} onClick={changeCurrentPage(-1)}>
-          {'<'}
+          <ArrowLeftIcon width={30} height={15} />
         </button>
         <button class={styles.button} onClick={changeCurrentPage(1)}>
-          {'>'}
+          <ArrowLeftIcon width={30} height={15} />
         </button>
       </div>
     </div>
---
+++ src/components/icons/ArrowLeftIcon.tsx
@@ -0,0 +1,13 @@
+import type { JSX } from 'solid-js/jsx-runtime';
+
+export const ArrowLeftIcon = (props: JSX.SvgSVGAttributes<SVGSVGElement>) => (
+  <svg
+    viewBox="0 0 29.771 29.771"
+    xmlns="http://www.w3.org/2000/svg"
+    width={30}
+    height={30}
+    {...props}
+  >
+    <path d="M29.77 14.89a3 3 0 0 1-3 3H11.5l4.5 4.5a3 3 0 0 1-4.24 4.25L0 14.88 11.74 3.14A3 3 0 0 1 16 7.38l-4.5 4.5h15.28a3 3 0 0 1 3 3z" />
+  </svg>
+);
